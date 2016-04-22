41916cb95b5e1a74b6ebdcb568026e3437d65e27
ylin <yuan.lin@zuora.com>
2016-04-22 12:52:57 +0800

    add phrases app

---
+++ phrases/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class phrase(models.Model):
+	phrase = models.CharField(max_length=500)
+	translated = models.CharField(max_length=1000)
+	comment = models.CharField(max_length=1000)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)	
